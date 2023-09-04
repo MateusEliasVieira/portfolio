5bfbbc4be079e90c12d16e89cd179e75d512c5e2
Mateus Elias Vieira <mateusifg16@gmail.com>
2023-09-04 11:15:30 -0300

    ajustado responsividade

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Footer from './componentes/footer/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{overflowX:'hidden'}}>
       <Header/>
       <Servico/>
       <Info/>
